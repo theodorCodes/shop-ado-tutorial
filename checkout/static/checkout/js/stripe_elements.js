3efc445b75ca9fc9eb605f4d006ee52086c997bc
theodorCodes <laphan.fan@gmail.com>
2021-11-14 00:42:31 +0800

    Added loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,4 +1,11 @@
-// Stripe elements
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -37,7 +44,6 @@ card.addEventListener('change', function (event) {
     }
 });
 
-
 // Handle form submit
 var form = document.getElementById('payment-form');
 
@@ -45,6 +51,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);  // HERE
+    $('#loading-overlay').fadeToggle(100);  // HERE
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -58,6 +66,8 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
