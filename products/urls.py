b20d150972d074636f9907510367075321e2833b
theodorCodes <laphan.fan@gmail.com>
2021-11-21 23:48:28 +0800

    Added delete products

--- products/urls.py
+++ products/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     # Edit product page
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    # Delete products
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
