419aede6efc067b83e5f6f05e82e041e6bfb7ab4
theodorCodes <laphan.fan@gmail.com>
2021-11-24 15:56:39 +0800

    Added Google Email

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -116,7 +116,9 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 # EMAIL_BACKEND by and required by allauth
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# See new email settings at the bottom of this file
+
 
 # EMAIL settings by Code Institute tutorial
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
@@ -264,4 +266,17 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 # Stripe webhooks
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # Email
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+DEFAULT_FROM_EMAIL = 'bettertryout@gmail.com'
+
+# New email settings
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
