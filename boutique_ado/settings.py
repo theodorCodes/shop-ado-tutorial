860b48830b4d104eace16bbeb6a0d87238821033
theodorCodes <laphan.fan@gmail.com>
2021-11-24 09:12:18 +0800

    Added cache settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -218,6 +218,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS bucket access
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # AWS S3 Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ci-boutique-ado-bucket'
     AWS_S3_REGION_NAME = 'eu-west-1'
